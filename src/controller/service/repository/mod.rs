3fc3e6f77e6c9e6ffe8ed8fa05ea6ad83dedf049
RafoDias <74609193+RafoDias@users.noreply.github.com>
2024-11-09 19:24:33 -0300

    repo id_generator

--- src/controller/service/repository/mod.rs
+++ src/controller/service/repository/mod.rs
@@ -5,3 +5,9 @@ pub const DB_ROOT_PATH: &str = "./databases/";
 pub fn get_database_path(repo_path: &str) -> String {
     return format!("{}{}", DB_ROOT_PATH, repo_path);
 }
+
+pub fn id_generator(repo_path: &str) -> String {
+    let db: sled::Db = sled::open(get_database_path(repo_path)).unwrap();
+    let id: String = db.generate_id().unwrap().to_string();
+    return id;
+}
