3844d44be4fd5a321b6d66f2bcbf2b58f4242ca6
RafoDias <74609193+RafoDias@users.noreply.github.com>
2024-11-23 21:25:30 -0300

    level i32 em character e implementacao de new

--- src/models/character.rs
+++ src/models/character.rs
@@ -1,11 +1,21 @@
 use chrono::{self, NaiveDate};
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, PartialEq, Clone)]
 pub struct Character {
-    pub id: String,
     pub name: String,
-    pub level: i8,
+    pub level: i32,
     pub class: super::Class,
     pub birthday: NaiveDate,
+}
+
+impl Character {
+    pub fn new(name: &str, level: i32, class: super::Class, birthday: NaiveDate) -> Character {
+        Character {
+            name: name.to_string(),
+            level,
+            class,
+            birthday,
+        }
+    }
 }
