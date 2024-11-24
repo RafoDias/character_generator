87f0aabb3ea75586041b99d6a41866732f0ae0f8
RafoDias <74609193+RafoDias@users.noreply.github.com>
2024-11-23 21:26:07 -0300

    enum Class converte de e para i32

--- src/models/class.rs
+++ src/models/class.rs
@@ -4,6 +4,7 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
 pub enum Class {
+    NoClass,
     Warrior,
     Cleric,
     Rogue,
@@ -12,6 +13,7 @@ pub enum Class {
 impl fmt::Display for Class {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
+            Class::NoClass => write!(f, "No Class"),
             Class::Warrior => write!(f, "Warrior"),
             Class::Cleric => write!(f, "Cleric"),
             Class::Rogue => write!(f, "Rogue"),
@@ -22,6 +24,7 @@ impl fmt::Display for Class {
 impl fmt::Debug for Class {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
+            Class::NoClass => write!(f, "No Class"),
             Class::Warrior => write!(f, "Warrior"),
             Class::Cleric => write!(f, "Cleric"),
             Class::Rogue => write!(f, "Rogue"),
@@ -32,9 +35,47 @@ impl fmt::Debug for Class {
 impl From<Class> for i32 {
     fn from(class: Class) -> i32 {
         match class {
+            Class::NoClass => 0,
             Class::Warrior => 1,
             Class::Cleric => 2,
             Class::Rogue => 3,
         }
     }
 }
+
+impl From<i32> for Class {
+    fn from(value: i32) -> Class {
+        match value {
+            0 => Class::NoClass,
+            1 => Class::Warrior,
+            2 => Class::Cleric,
+            3 => Class::Rogue,
+            _ => Class::NoClass,
+        }
+    }
+}
+
+impl PartialEq for Class {
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (Class::NoClass, Class::NoClass) => true,
+            (Class::Warrior, Class::Warrior) => true,
+            (Class::Cleric, Class::Cleric) => true,
+            (Class::Rogue, Class::Rogue) => true,
+            _ => false,
+        }
+    }
+}
+
+impl Clone for Class {
+    fn clone(&self) -> Self {
+        match self {
+            Class::NoClass => Class::NoClass,
+            Class::Warrior => Class::Warrior,
+            Class::Cleric => Class::Cleric,
+            Class::Rogue => Class::Rogue,
+        }
+    }
+}
+
+impl Copy for Class {}
