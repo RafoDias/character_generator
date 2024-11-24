a8896cfa273a43b53709a8ee8f96a07676cd5c85
RafoDias <74609193+RafoDias@users.noreply.github.com>
2024-11-24 16:49:00 -0300

    display, debug e get_empty_character para Character entity

--- src/models/character.rs
+++ src/models/character.rs
@@ -1,7 +1,7 @@
 use chrono::{self, NaiveDate};
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Serialize, Deserialize, PartialEq, Clone)]
+#[derive(Serialize, Deserialize, PartialEq, Clone)]
 pub struct Character {
     pub name: String,
     pub level: i32,
@@ -18,4 +18,33 @@ impl Character {
             birthday,
         }
     }
+}
+
+impl std::fmt::Display for Character {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(
+            f,
+            "Character: {{\n\tName: {},\n\tLevel: {},\n\tClass: {},\n\tBirthday: {}\n}}",
+            self.name, self.level, self.class, self.birthday
+        )
+    }
+}
+
+impl std::fmt::Debug for Character {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(
+            f,
+            "Character: {{\n\tName: {},\n\tLevel: {},\n\tClass: {},\n\tBirthday: {}\n}}",
+            self.name, self.level, self.class, self.birthday
+        )
+    }
+}
+
+pub fn get_empty_caracter() -> Character {
+    return Character {
+        name: "".to_string(),
+        level: 0,
+        class: super::Class::NoClass,
+        birthday: NaiveDate::from_ymd_opt(1, 1, 1).unwrap(),
+    };
 }
